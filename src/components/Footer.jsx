28e098cbeb0362c3d6b2a62232d16c764fa9a90d
Fran <frandiezdev@gmail.com>
2018-12-17 23:58:25 -0300

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render () {
+    return (
       <footer class="footer footer-light bg-light">
         <div class="container py-4">
           <span class="text-muted">www.frandiez.tech - &copy; { new Date().getFullYear() }</span>
